0b3db63ae60427975c3b02a79f0f28035e5112ab
admin <admin@sybbjs.idazui>
2017-03-21 16:27:15 +0800

    PY-2016.3.2 <admin@sybbjs Create IntelliLang.xml, ide.general.xml, git.xml, project.default.xml, diff.xml, other.xml, file.template.settings.xml, find.xml, ui.lnf.xml, filetypes.xml, keymap.xml, Python Script.py, code.style.schemes.xml, colors.scheme.xml, databaseDrivers.xml, conda_packages.xml, sh.sh, Default.xml, hg.xml, laf.xml, jdk.table.xml, Monokai copy.icls, web-browsers.xml, editor.codeinsight.xml, databaseSettings.xml, bash.xml, editor.xml, webServers.xml, debugger.xml, bash.icls

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# ecoding=utf8
+# charset :utf8
+# mail:362805276@qq.com
+import sys,os
+os.environ['NLS_LANG'] = 'AMERICAN_AMERICA.AL32UTF8'
+reload(sys)
+sys.setdefaultencoding("utf8")
---
+++ fileTemplates/sh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#The scripts is used to:
+#create by sybbjs mail:1076296613@qq.com
+#date : 2015-05-07
